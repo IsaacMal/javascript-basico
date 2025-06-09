798f74681cb19a5b95f1ab97e1e89f5e9dfcc85b
Isaac Maldonado <imaldonadom@est.ups.edu.ec>
2025-06-09 16:44:26 -0500

    Variables y consola

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
 function mostrarMensaje(){
     alert("Hola Mundo");
 }
+
+//Variables
+function imprimirVariables(){
+    let nombre = "Rodrigo";
+    let edad = 42
+    console.log("nombre:", nombre)
+    console.log("edad:", edad)
+    //Forma moderna de concatenar cadenas
+    console.log(`nombre: ${nombre}, edad ${edad}`)
+    //Otros tipos de impresion
+    console.error("Error")
+    console.warn("Advertencia")
+}
+
+function arreglos{
+    let frutas = ['manzana','pera','banana'];
+    for(let f of frutas)
+        
+}
