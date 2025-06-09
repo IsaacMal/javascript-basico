5aaa510712d6bd34feb64ee32080935270d638bf
Isaac Maldonado <imaldonadom@est.ups.edu.ec>
2025-06-09 16:54:18 -0500

    Arreglos,bucles y condicional

--- script.js
+++ script.js
@@ -16,8 +16,17 @@ function imprimirVariables(){
     console.warn("Advertencia")
 }
 
-function arreglos{
+function arreglos(){
+    //Arreglos
     let frutas = ['manzana','pera','banana'];
-    for(let f of frutas)
-        
+    //Bucle
+    for(let f of frutas){
+        console.log(f)
+    }
+    //Condicionales
+    if (frutas[1] === 'pera'){
+        console.log("La segunda fruta es una pera");
+    }else{
+     console.log('No es una pera');
+    }
 }
